d112a92b330995ee7639de6ba76d342ff77a8edd
cirzear <cirzear@163.com>
2019-06-16 21:52:41 +0800

    6.16update2

--- WUIF1905/6.14/js/index_jq.js
+++ WUIF1905/6.14/js/index_jq.js
@@ -1,58 +1,90 @@
 $(function () {
+    let prevBtn = $('.button-prev');
+    let nextBtn = $('.button-next');
+    let btns = $('.pagination > ul > li');
+    let imgs = $('.lb_img');
     let current = 0, next = 0;
-    let btns = $("li");
-    let imgs = $('.wrapper > .lb_img');
-    let prev = $(".prev_button");
-    let nexts = $(".next_button");
     let flag = true;
     let w = imgs[0].offsetWidth;
-    console.log(imgs.length, w);
-    prev.click(function () {
-        if (!flag){
+
+    imgs.eq(0).css({'left':0});
+    nextBtn.click(function () {
+        if (!flag) {
             return;
         }
-        flag =false;
+        flag = false;
         next++;
         if (next == imgs.length) {
             next = 0;
         }
-        imgs[next].css({'left':w});
-        imgs[current].animate({left: -w});
-        imgs[next].animate({left: 0} ,function () {
-            flag =true;
+        $(imgs[next]).css({left: w});
+        $(imgs[current]).animate({left: -w});
+        $(imgs[next]).animate({left: 0}, function () {
+            flag = true;
         });
+        btns.eq(next).addClass('point').end().eq(current).removeClass('point');
         current = next;
 
     });
-    nexts.click(function () {
-        if (!flag){
+    prevBtn.click(function () {
+        if (!flag) {
             return;
         }
-        flag =false;
+        flag = false;
         next--;
         if (next < 0) {
             next = imgs.length - 1;
         }
-        imgs[next].style.left = -w + 'px';
-        imgs[current].animate({left: w});
-        imgs[next].animate({left: 0} ,function () {
-            flag =true;
+        $(imgs[next]).css({left: -w});
+        $(imgs[current]).animate({left: w});
+        $(imgs[next]).animate({left: 0}, function () {
+            flag = true;
         });
+        btns.eq(next).addClass('point').end().eq(current).removeClass('point');
         current = next;
-
     });
 
-    let t = setInterval(function () {
-        nexts.click();
-    }, 2500);
+////////////////点//////////////////
+    for(let i =0; i<btns.length; i++){
+        // console.log($(btns[i]));
+        btns[i].click = function() {
+            if (current === i){
+                return;
+            }
+            next = i;
+            if(current < next){
+                imgs[next].css({'left':w});
+                imgs[current].animate({left:-w});
+                imgs[next].animate({left:-w});
+            }else {
+                imgs[next].css({'left': -w});
+                imgs[current].animate({left: w});
+                imgs[next].animate({left: 0});
+            }
+            current = next;
 
-    imgs[0].mouseenter = function () {
+        }
+
+
+    }
+
+    
+    let bp = $(".container");
+    let t = setInterval(function () {
+        nextBtn.click();
+    }, 1000);
+
+    bp[0].onmouseover = function () {
         clearInterval(t)
     };
-    imgs[0].mouseleave = function () {
+
+    bp[0].onmouseout = function () {
         t = setInterval(function () {
-            nexts.click();
-        }, 2500);
+            nextBtn.click();
+        }, 1000);
     };
 
-});
+
+
+
+})
